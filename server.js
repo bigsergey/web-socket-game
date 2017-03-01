f89b4ecd876c2c8b7e6e3ccd90c31a5277872936
Patryk Poleszczuk <patryk.poleszczuk@gmail.com>
2017-03-01 10:15:27 +0100

    configure server

--- server.js
+++ server.js
@@ -1,32 +1,21 @@
 const webSocketsServerPort = 1337;
 const WebSocketServer = require('websocket').server;
 const http = require('http');
-
-const server = http.createServer(function(request, response) {
-  // Not important for us. We're writing WebSocket server, not HTTP server
-});
-server.listen(webSocketsServerPort, function() {
-  console.log((new Date()) + " Server is listening on port " + webSocketsServerPort);
-});
+const chalk = require('chalk');
+const log = console.log;
 
 // create the server
-wsServer = new WebSocketServer({
-  httpServer: server
+server = new WebSocketServer({
+  httpServer: http.createServer().listen(webSocketsServerPort, () => {
+    log(chalk.green(`Server is listening on port: ${webSocketsServerPort}`));
+  })
 });
 
-// WebSocket server
-wsServer.on('request', function(request) {
+server.on('request', (request) => {
   const connection = request.accept(null, request.origin);
+  log(chalk.yellow(`connections: ${connection.socket.server.connections}`));
 
-  // This is the most important callback for us, we'll handle
-  // all messages from users here.
-  connection.on('message', function(message) {
-    if (message.type === 'utf8') {
-      // process WebSocket message
-    }
-  });
-
-  connection.on('close', function(connection) {
-    // close user connection
+  connection.on('close', () => {
+    log(chalk.yellow(`connections: ${connection.socket.server.connections}`));
   });
 });
