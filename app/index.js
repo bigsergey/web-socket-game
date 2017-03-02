922d07340041e6b66207f15aebb57d72d4e5aba7
Patryk Poleszczuk <patryk.poleszczuk@gmail.com>
2017-03-02 16:12:42 +0100

    change websocket-node to ws library and move game logic to server side

--- app/index.js
+++ app/index.js
@@ -3,15 +3,14 @@ import {toNumber} from 'lodash';
 import GameLogic from './gameLogic';
 
 const WEB_SOCKET_ADDRESS = 'ws://127.0.0.1:1337';
+const NUMBER_OF_FIELDS = 9;
 
 // if user is running mozilla then use it's built-in WebSocket
 window.WebSocket = window.WebSocket || window.MozWebSocket;
 
 const connection = new WebSocket(WEB_SOCKET_ADDRESS);
-const simpleMessage = JSON.stringify({a: 'Patryk'});
 
 connection.onopen = function () {
-  connection.send(JSON.stringify(simpleMessage));
   // connection is opened and ready to use
 };
 
@@ -19,23 +18,11 @@ connection.onerror = function (error) {
   // an error occurred when sending/receiving data
 };
 
-connection.onmessage = function (message) {
-  // try to decode json (I assume that each message from server is json)
-  try {
-    const json = JSON.parse(message.data);
-    console.log(json);
-  } catch (e) {
-    console.log('This doesn\'t look like a valid JSON: ', message.data);
-    return;
-  }
-  // handle incoming message
-};
-
 
 
 // ---------- Create Game Board ----------
 
-const fieldsArray = Array.from(Array(9).keys());
+const fieldsArray = Array.from(Array(NUMBER_OF_FIELDS).keys());
 const $container = document.querySelector('#container');
 const $app = document.querySelector('#app');
 
@@ -45,35 +32,48 @@ fieldsArray.forEach(item => {
 
 
 // ---------- Game ----------
-
+let gameInfo = null;
 const $fields = document.getElementsByClassName('field');
-const firstPlayerMoves = [], secondPlayerMoves = [];
-let isFirstPlayerMove = true;
+
+const drawOpponentMoves = (opponentMoves) => {
+  console.log(opponentMoves);
+};
+
+
+connection.onmessage = (message) => {
+  try {
+    const json = JSON.parse(message.data);
+    gameInfo = json;
+    drawOpponentMoves(gameInfo.opponentMoves);
+  } catch (e) {
+    console.log('This doesn\'t look like a valid JSON: ', message.data);
+    return;
+  }
+};
 
 for(let i = 0; i < $fields.length; i++) {
   $fields[i].addEventListener('click', (e) => {
+    console.log(gameInfo);
     if (!e.target.innerHTML) {
-      if (isFirstPlayerMove) {
-        firstPlayerMoves.push(toNumber(e.target.id));
-        e.target.innerHTML = 'X';
-        isFirstPlayerMove = false;
-      } else {
-        secondPlayerMoves.push(toNumber(e.target.id));
-        e.target.innerHTML = 'O';
-        isFirstPlayerMove = true;
+      if (gameInfo.isPlayerMove) {
+        gameInfo.playerMoves.push(toNumber(e.target.id));
+        e.target.innerHTML = gameInfo.playerSymbol;
+        gameInfo.isPlayerMove = false;
       }
     }
 
-    console.log(firstPlayerMoves, secondPlayerMoves);
-    if (GameLogic.checkPlayerMoves(firstPlayerMoves)) {
-      $app.insertAdjacentHTML('afterbegin', `<div class="overlay">First player won!</div>`);
-    }
-    if (GameLogic.checkPlayerMoves(secondPlayerMoves)) {
-      $app.insertAdjacentHTML('afterbegin', `<div class="overlay">Second player won!</div>`);
-    }
+    connection.send(JSON.stringify(gameInfo));
 
-    if (firstPlayerMoves.length + secondPlayerMoves.length === 9 && !GameLogic.checkPlayerMoves(firstPlayerMoves) && !GameLogic.checkPlayerMoves(secondPlayerMoves)) {
-      $app.insertAdjacentHTML('afterbegin', `<div class="overlay">Draw!</div>`);
-    }
+    // console.log(firstPlayerMoves, secondPlayerMoves);
+    // if (GameLogic.checkPlayerMoves(firstPlayerMoves)) {
+    //   $app.insertAdjacentHTML('afterbegin', `<div class="overlay">First player won!</div>`);
+    // }
+    // if (GameLogic.checkPlayerMoves(secondPlayerMoves)) {
+    //   $app.insertAdjacentHTML('afterbegin', `<div class="overlay">Second player won!</div>`);
+    // }
+    //
+    // if (firstPlayerMoves.length + secondPlayerMoves.length === 9 && !GameLogic.checkPlayerMoves(firstPlayerMoves) && !GameLogic.checkPlayerMoves(secondPlayerMoves)) {
+    //   $app.insertAdjacentHTML('afterbegin', `<div class="overlay">Draw!</div>`);
+    // }
   })
 }
--- server.js
+++ server.js
@@ -1,37 +1,77 @@
 const webSocketsServerPort = 1337;
-const WebSocketServer = require('websocket').server;
+const WebSocketServer = require('ws').Server;
 const http = require('http');
+const {intersection} = require('lodash');
 const chalk = require('chalk');
 const log = console.log;
 
-// create the server
-server = new WebSocketServer({
-  httpServer: http.createServer().listen(webSocketsServerPort, () => {
-    log(chalk.green(`Server is listening on port: ${webSocketsServerPort}`));
-  })
-});
 
-const simpleData = JSON.stringify({
-  x: 'x',
-  o: 'o'
+server = new WebSocketServer({port: webSocketsServerPort}, () => {
+  log(chalk.green(`Server is listening on port: ${webSocketsServerPort}`));
 });
 
 let connectionsCounter = 0;
+let playerOne = {
+  'id': 1,
+  'isPlayerMove': true,
+  'playerMoves': [],
+  'opponentMoves': [],
+  'playerSymbol': 'X',
+};
+let playerTwo = {
+  'id': 2,
+  'isPlayerMove': false,
+  'playerMoves': [],
+  'opponentMoves': [],
+  'playerSymbol': 'O',
+};
 
-server.on('request', (request) => {
-  const connection = request.accept(null, request.origin);
+server.on('connection', (ws) => {
   connectionsCounter++;
-
   log(chalk.yellow(`connections: ${connectionsCounter}`));
 
-  connection.sendUTF('asd');
-  connection.send(simpleData);
-
-  connection.on('message', (data) => {
-    log(chalk.blue(JSON.parse(data.utf8Data)));
+  server.clients.forEach(client => {
+    if (client._ultron.id === 1) {
+      client.send(JSON.stringify(playerOne));
+    }
+    if (client._ultron.id === 2) {
+      client.send(JSON.stringify(playerTwo));
+    }
   });
 
-  connection.on('close', () => {
+  ws.on('message', (msg) => {
+    log(chalk.blue(msg));
+    if (msg['id'] === 1) {
+      server.clients.forEach(client => {
+        if (client._ultron.id === 1) {
+          playerOne.isPlayerMove = msg['isPlayerMove'];
+          playerOne.playerMoves = msg['playerMoves'];
+          client.send(JSON.stringify(playerOne));
+        }
+        if (client._ultron.id === 2) {
+          playerTwo.isPlayerMove = !msg['isPlayerMove'];
+          playerTwo.playerMoves = msg['opponentMoves'];
+          client.send(JSON.stringify(playerTwo));
+        }
+      });
+    }
+    if (msg['id'] === 2) {
+      server.clients.forEach(client => {
+        if (client._ultron.id === 1) {
+          playerOne.isPlayerMove = !msg['isPlayerMove'];
+          playerOne.playerMoves = msg['opponentMoves'];
+          client.send(JSON.stringify(playerOne));
+        }
+        if (client._ultron.id === 2) {
+          playerTwo.isPlayerMove = msg['isPlayerMove'];
+          playerTwo.playerMoves = msg['playerMoves'];
+          client.send(JSON.stringify(playerTwo));
+        }
+      });
+    }
+  });
+
+  ws.on('close', () => {
     connectionsCounter--;
     log(chalk.yellow(`connections: ${connectionsCounter}`));
   });
