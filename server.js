2be21478baf4f0311e1319d806443d7cf87cb7ca
Patryk Poleszczuk <patryk.poleszczuk@gmail.com>
2017-03-03 15:20:42 +0100

    change server logic

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const WebSocketServer = require('ws').Server;
 const http = require('http');
 const chalk = require('chalk');
 const log = console.log;
+const {remove, drop} = require('lodash');
 
 const FIRST_PLAYER_SYMBOL = 'X';
 const SECOND_PLAYER_SYMBOL = 'O';
@@ -11,71 +12,72 @@ server = new WebSocketServer({port: webSocketsServerPort}, () => {
   log(chalk.green(`Server is listening on port: ${webSocketsServerPort}`));
 });
 
-let connectionsCounter = 0;
+
+let clientsArray = [];
 const playerOne = {
-  'id': 1,
+  'id': 0,
   'isPlayerMove': true,
   'playerMoves': [],
   'opponentMoves': [],
   'playerSymbol': FIRST_PLAYER_SYMBOL,
   'opponentSymbol': SECOND_PLAYER_SYMBOL,
+  'waitingForOtherPlayer': true,
 };
 const playerTwo = {
-  'id': 2,
+  'id': 0,
   'isPlayerMove': false,
   'playerMoves': [],
   'opponentMoves': [],
   'playerSymbol': SECOND_PLAYER_SYMBOL,
   'opponentSymbol': FIRST_PLAYER_SYMBOL,
+  'waitingForOtherPlayer': true,
 };
 
 server.on('connection', (ws) => {
-  connectionsCounter++;
-  log(chalk.yellow(`connections: ${connectionsCounter}`));
+  clientsArray.push(ws);
+  log(chalk.yellow(`connections: ${clientsArray.length}`));
 
-  server.clients.forEach(client => {
-    if (client._ultron.id === 1) {
-      client.send(JSON.stringify(playerOne));
-    }
-    if (client._ultron.id === 2) {
-      client.send(JSON.stringify(playerTwo));
-    }
-  });
+  if (clientsArray[0]) {
+    playerOne.id = clientsArray[0]._ultron.id;
+    clientsArray[0].send(JSON.stringify(playerOne));
+  }
+  if (clientsArray[1]) {
+    playerTwo.id = clientsArray[1]._ultron.id;
+    clientsArray[1].send(JSON.stringify(playerTwo));
+  }
 
   ws.on('message', (data) => {
     const gameInfo = JSON.parse(data);
-    if (gameInfo.id === 1) {
-      server.clients.forEach(client => {
-        if (client._ultron.id === 1) {
-          playerOne.isPlayerMove = gameInfo.isPlayerMove;
-          playerOne.playerMoves = gameInfo.playerMoves;
-          client.send(JSON.stringify(playerOne));
-        }
-        if (client._ultron.id === 2) {
-          playerTwo.isPlayerMove = !gameInfo.isPlayerMove;
-          playerTwo.opponentMoves = gameInfo.playerMoves;
-          client.send(JSON.stringify(playerTwo));
-        }
-      });
+    if (gameInfo.id === playerOne.id) {
+      if (clientsArray[0]) {
+        playerOne.isPlayerMove = gameInfo.isPlayerMove;
+        playerOne.playerMoves = gameInfo.playerMoves;
+        clientsArray[0].send(JSON.stringify(playerOne));
+      }
+      if (clientsArray[1]) {
+        playerTwo.isPlayerMove = !gameInfo.isPlayerMove;
+        playerTwo.opponentMoves = gameInfo.playerMoves;
+        clientsArray[1].send(JSON.stringify(playerTwo));
+      }
     }
-    if (gameInfo.id === 2) {
-      server.clients.forEach(client => {
-        if (client._ultron.id === 1) {
-          playerOne.isPlayerMove = !gameInfo.isPlayerMove;
-          playerOne.opponentMoves = gameInfo.playerMoves;
-          client.send(JSON.stringify(playerOne));
-        }
-        if (client._ultron.id === 2) {
-          playerTwo.isPlayerMove = gameInfo.isPlayerMove;
-          playerTwo.playerMoves = gameInfo.playerMoves;
-          client.send(JSON.stringify(playerTwo));
-        }
-      });
+    if (gameInfo.id === playerTwo.id) {
+      if (clientsArray[0]) {
+        playerOne.isPlayerMove = !gameInfo.isPlayerMove;
+        playerOne.opponentMoves = gameInfo.playerMoves;
+        clientsArray[0].send(JSON.stringify(playerOne));
+      }
+      if (clientsArray[1]) {
+        playerTwo.isPlayerMove = gameInfo.isPlayerMove;
+        playerTwo.playerMoves = gameInfo.playerMoves;
+        clientsArray[1].send(JSON.stringify(playerTwo));
+      }
     }
   });
 
   ws.on('close', () => {
-    connectionsCounter--;
-    log(chalk.yellow(`connections: ${connectionsCounter}`));
+    clientsArray = remove(clientsArray, client => {
+      return client._ultron;
+    });
+    log(chalk.yellow(`connections: ${clientsArray.length}`));
   });
 });
